9a213024e36566cba246f0316fcfcd011280aea7
LeemaJosephine <leemajosephine3@gmail.com>
2026-06-08 08:58:28 +0530

    Day-24

--- src/test/java/day23/TC001_LoginTest_Valid.java
+++ src/test/java/day23/TC001_LoginTest_Valid.java
@@ -5,6 +5,10 @@ import java.io.IOException;
 import java.time.Duration;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
@@ -16,6 +20,8 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
@@ -24,6 +30,13 @@ public class TC001_LoginTest_Valid {
 
 	
 	public static WebDriver driver;
+	public String sheetName;
+	
+	@BeforeTest
+	public void setup_Test() {
+		
+		sheetName="LoginData";
+	}
 	
 	@Parameters({"browser","url"})
 	@BeforeMethod
@@ -43,53 +56,105 @@ public class TC001_LoginTest_Valid {
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 	}
+	
+	
+	@DataProvider
+	public String[][] readData() throws IOException {
+		
+		return readDataFromExcel(sheetName);
+	}
+	
+	public static void takeScreenShot(String screenShotName) throws IOException {
+				
+				// Convert webdriver object to take screenshot
+				
+				TakesScreenshot screenshot = ((TakesScreenshot)driver);
+				
+				// call the getScreenshotAs method to create the image file
+				
+				File src = screenshot.getScreenshotAs(OutputType.FILE);
+				
+				// Mention the destination
+				File dest = new File("C:\\Users\\leema\\eclipse-workspace\\SelenimProject\\src\\test\\java\\screenshot\\"+screenShotName+".png");
+				
+				// Copy from source to destination
+				
+				FileUtils.copyFile(src, dest);
+	}
 
-	@Test
-	public void login_Test() throws IOException, InterruptedException {
+	public static String[][] readDataFromExcel(String sheetname) throws IOException {
+		
+				System.out.println(sheetname);
+				// Open the workbook
+				XSSFWorkbook book = new XSSFWorkbook("C:\\Users\\leema\\eclipse-workspace\\SelenimProject\\src\\test\\resources\\TestData\\TestData_OrangeHRM.xlsx");
+				
+				// Open the sheet
+				XSSFSheet sheet = book.getSheet(sheetname);
+				
+				// Get the no.of rows
+				int rowCount = sheet.getLastRowNum();
+				
+				// Get the column count
+				int columnCount = sheet.getRow(0).getLastCellNum();
+				
+				//Create 2D Array
+				String[][] data = new String[rowCount][columnCount];
+				
+				// Get into each row
+				
+				for(int i=1; i<=rowCount; i++) {
+					
+					XSSFRow row = sheet.getRow(i);
+					
+					// get into the column
+					
+					for(int j=0; j<columnCount; j++) {
+						
+						XSSFCell cell = row.getCell(j);
+						
+						// to store in array
+						
+						data[i-1][j]=cell.getStringCellValue();
+					}
+					
+				}
+				book.close();
+				return data;
+				
+	}
+	
+	
+	@Test(dataProvider = "readData")
+	public void login_Test(String user_name,String pass, String test_type, String expectedText) throws IOException, InterruptedException {
 		// TODO Auto-generated method stub	
 		WebElement username = driver.findElement(By.name("username"));
-		username.sendKeys("Admin");
+		username.sendKeys(user_name);
 		
 		WebElement password = driver.findElement(By.name("password"));
-		password.sendKeys("admin123");
+		password.sendKeys(pass);
 		
 		driver.findElement(By.xpath("//button[text()=' Login ']")).click();
-		
+				
 		Thread.sleep(3000);
 		
-		// To take screenshot
-		
-		// Convert webdriver object to take screenshot
-		
-		TakesScreenshot screenshot = ((TakesScreenshot)driver);
-		
-		// call the getScreenshotAs method to create the image file
-		
-		File src = screenshot.getScreenshotAs(OutputType.FILE);
-		
-		// Mention the destination
-		File dest = new File("C:\\Users\\leema\\eclipse-workspace\\SelenimProject\\src\\test\\java\\screenshot\\ValidloginTest.png");
-		
-		// Copy from source to destination
-		
-		FileUtils.copyFile(src, dest);
+		takeScreenShot("Valid_Login");
 		
 		// Assertion
-		
-		String expectedText ="Dashboa";
-		
-		String actualText = driver.findElement(By.xpath("//h6[text()='Dashboard']")).getText();
-		
-		// Hard Assertion
-		
-		// Assert.assertEquals(actualText, expectedText);
+		if(test_type.equalsIgnoreCase("ValidUnValidPass")) {
+			
+			String actualText = driver.findElement(By.xpath("//h6[text()='Dashboard']")).getText();
+			Assert.assertEquals(actualText, expectedText);
+			
+		} else if(test_type.equalsIgnoreCase("ValidUnInvalidPass") || test_type.equalsIgnoreCase("InvalidUnValidPass") || test_type.equalsIgnoreCase("InvalidUnInvalidPass") ) {
+			
+			String errorText = driver.findElement(By.xpath("//p[text()='Invalid credentials']")).getText();
+			Assert.assertEquals(errorText, expectedText); 
+		}
 		
 		// Soft Assertion
-		
-		SoftAssert assertObj = new SoftAssert();
-		assertObj.assertEquals(actualText, expectedText);
-		assertObj.assertAll();  //causes and exception when assert fails
-		
+//		SoftAssert assertObj = new SoftAssert();
+//		assertObj.assertEquals(actualText, expectedText);
+//		assertObj.assertAll();  //causes and exception when assert fails	
 	}
 	
 	@AfterMethod
